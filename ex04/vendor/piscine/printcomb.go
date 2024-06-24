9e90c52045d41b8f6c691974290ae0ea50073ca1
Neko-Sato <hisatosi35350@gmail.com>
2024-06-24 22:20:35 +0900

    ex04

--- ex04/vendor/piscine/printcomb.go
+++ ex04/vendor/piscine/printcomb.go
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 21:18:01 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/06/24 21:48:27 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/06/24 22:08:45 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@ import "ft"
 
 func PrintComb() {
 	isfirst := true
-	for a := 0; a <= 9; a++ {
-		for b := a + 1; b <= 9; b++ {
-			for c := b + 1; c <= 9; c++ {
+	for a := 0; a < 10; a++ {
+		for b := a + 1; b < 10; b++ {
+			for c := b + 1; c < 10; c++ {
 				if isfirst {
 					isfirst = false
 				} else {
 					ft.PrintRune(',')
+					ft.PrintRune(' ')
 				}
 				ft.PrintRune(rune('0' + a))
 				ft.PrintRune(rune('0' + b))
